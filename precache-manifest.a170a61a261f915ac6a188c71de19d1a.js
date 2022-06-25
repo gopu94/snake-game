f32925c75c00bfc8ac74a2b556620e4b9f6ec855
Shree Nair <gopu9363@gmail.com>
2022-06-25 11:08:49 +0530

    Updates

---
+++ precache-manifest.a170a61a261f915ac6a188c71de19d1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c68c6df5ee7c0bbfdde3",
+    "url": "/snake-game/static/css/main.12148065.chunk.css"
+  },
+  {
+    "revision": "c68c6df5ee7c0bbfdde3",
+    "url": "/snake-game/static/js/main.5c89be10.chunk.js"
+  },
+  {
+    "revision": "9a755d0ffde03a58c68a",
+    "url": "/snake-game/static/js/runtime~main.356adb96.js"
+  },
+  {
+    "revision": "9d4c0d6ec62814ec342a",
+    "url": "/snake-game/static/js/2.775b71d9.chunk.js"
+  },
+  {
+    "revision": "1b9853913761ce4039647a1a4b44385c",
+    "url": "/snake-game/index.html"
+  }
+];
